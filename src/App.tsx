7dfa78a3eae3231a590a883aa2d3a7e62a17ceca
Shaun Church <shaun@shaunchurch.com>
2023-12-22 10:26:59 +0000

    Fill screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import { SearchForm } from "./components/SearchForm";
 
 export function App() {
   return (
-    <>
-      <header className="pl-20 p-3 drag-handle text-center bg-blue-950">
+    <div className="h-screen flex flex-col">
+      <header className="p-3 drag-handle text-center bg-zinc-950 border-b border-zinc-900 shadow-2xl">
         ChatGPT History Search
       </header>
-      <SearchForm />
-      <LoadButton />
-    </>
+      <div className="flex-1 overflow-auto">
+        <SearchForm />
+        <LoadButton />
+      </div>
+      {/* <footer className="p-1 text-center bg-zinc-950 border-t border-zinc-900 text-zinc-600">
+        Footer
+      </footer> */}
+    </div>
   );
 }
